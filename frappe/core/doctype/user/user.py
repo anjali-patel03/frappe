5276785cb8c34377ff0e8e33dcc3d2067de43d65
Anjali Patel <anjali.patel@artemhealthtech.com>
2026-01-28 11:31:17 +0530

    user: extend user doctype with organization-based controls

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -75,6 +75,7 @@ class User(Document):
 		bulk_actions: DF.Check
 		bypass_restrict_ip_check_if_2fa_enabled: DF.Check
 		code_editor_type: DF.Literal["vscode", "vim", "emacs"]
+		company: DF.Link | None
 		dashboard: DF.Check
 		default_app: DF.Literal[None]
 		default_workspace: DF.Link | None
